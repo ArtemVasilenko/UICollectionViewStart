d972955868e6ce280d1ce951b9d30207db9eda56
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-28 12:46:21 +0300

    +

--- UICollectionViewStart/MyCollectionViewCell.swift
+++ UICollectionViewStart/MyCollectionViewCell.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+
 class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellLabel: UILabel!
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+    var menu: Menu? {
+        didSet {
+            cellLabel.text = menu?.name
+            if let image = menu?.imagename {
+                cellImageView.image = UIImage(named: image)
+            }
+        }
+    }
     
 }
--- UICollectionViewStart/ViewController.swift
+++ UICollectionViewStart/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    var itemMenuArray: [Menu] = {
+        var menu = Menu(name: "Fanta", imagename: "fanta")
+        var menu2 = Menu(name: "Cola", imagename: "cola")
+        var menu3 = Menu(name: "Cola", imagename: "cola")
+        return [menu, menu2, menu3]
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.myCollectionView.dataSource = self
+        self.myCollectionView.delegate = self
     }
+}
 
-
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return itemMenuArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        if let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? MyCollectionViewCell {
+            
+            cell.menu = itemMenuArray[indexPath.row]
+            
+            
+          return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
 }
 
