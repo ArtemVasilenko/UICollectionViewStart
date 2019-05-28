b26605eac18e6a217afedcd5897cce1810c5db8d
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-28 10:41:09 +0300

    start

---
+++ UICollectionViewStart/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  UICollectionViewStart
+//
+//  Created by Артем on 5/28/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+}
