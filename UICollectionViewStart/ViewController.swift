10ab863200cf621339954e41a7c20ae1f181b536
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-05-28 10:36:42 +0300

    Initial Commit

---
+++ UICollectionViewStart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewStart
+//
+//  Created by Артем on 5/28/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
